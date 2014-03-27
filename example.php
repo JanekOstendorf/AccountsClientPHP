3e2ececaa3077767989e14ea18439ab6d20ffc32
ozzyfant <github@ozzyfant.de>
2014-03-27 15:36:34 +0100

    Add function example

--- example.php
+++ example.php
@@ -8,10 +8,20 @@
 require_once 'vendor/autoload.php';
 
 $profileRepo = new \AccountsClientPHP\HTTPProfileRepository();
-
-$criteria = new \AccountsClientPHP\ProfileCriteria('ozzy2345', 'minecraft');
-
+$criteria = new \AccountsClientPHP\ProfileCriteria('ozzy2345', 'minecraft'); // The Agent is kind of hard-coded in the API
 $profiles = $profileRepo->findProfilesByCriteria($criteria);
 
 var_dump($profiles[0]->getName()); // ozzy2345
 var_dump($profiles[0]->getID());   // c709e602680b40459dbe00e47f3f4b4b
+
+/*
+ * Example of a simple function
+ */
+
+function minecraftNameToID($minecraftName) {
+	$profileRepo = new \AccountsClientPHP\HTTPProfileRepository();
+	$criteria = new \AccountsClientPHP\ProfileCriteria($minecraftName, 'minecraft');
+	return $profileRepo->findProfilesByCriteria($criteria)[0]->getName();
+}
+
+var_dump(minecraftNameToID('ozzy2345')); // c709e602680b40459dbe00e47f3f4b4b
